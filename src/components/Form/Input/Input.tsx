e66f2b98f9f57db490ec74f853792b3be490eec0
JohnE03 <2101519@eng.asu.edu.eg>
2025-09-04 01:51:55 +0300

    Email availability check

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -7,19 +7,35 @@ type TInputProps<TFieldValue extends FieldValues> = {
     type?: string;
     register: UseFormRegister<TFieldValue>;
     error: string;
+    onBlur?: (e:React.FocusEvent<HTMLInputElement>)=>void;
+    formText?: string;
+    success?: string;
+    disabled?: boolean;
 }
 
 const Input =<TFieldValue extends FieldValues> ({
-    label, type="text", register, name, error
+    label, type="text", register, name, error, onBlur, formText, success, disabled
     }: TInputProps<TFieldValue>) => {
-  return (
-    <Form.Group className="mb-3">
-        <Form.Label>{label}</Form.Label>
-        <Form.Control type={type} {...register(name)}
-        isInvalid={error ? true: false}/>
-        <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
-    </Form.Group>
-  )
+        const onBlurHandler = (e: React.FocusEvent<HTMLInputElement>)=>{
+            if(onBlur){
+                onBlur(e);
+                register(name).onBlur(e);
+            }else{
+                register(name).onBlur(e);
+            }
+        }
+    return (
+        <Form.Group className="mb-3">
+            <Form.Label>{label}</Form.Label>
+            <Form.Control type={type} {...register(name)}
+            onBlur={onBlurHandler} isInvalid={error ? true: false}
+            isValid={success? true: false}
+            disabled={disabled}/>
+            <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
+            <Form.Control.Feedback type="valid">{success}</Form.Control.Feedback>
+            {formText && <Form.Text muted>{formText}</Form.Text>}
+        </Form.Group>
+    )
 }
 
 export default Input
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,40 +1,120 @@
 import { useForm, type SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {signUpSchema, type signUpType} from "@validations/signUpSchema";
+import { signUpSchema, type signUpType } from "@validations/signUpSchema";
+import useCheckEmailAvailability from "@hooks/useCheckEmailAvailability";
 import { Heading } from "@components/common";
 import { Input } from "@components/Form";
-import {Form, Button, Row, Col} from "react-bootstrap";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 const Register = () => {
-    const { register, handleSubmit, formState: {errors} } = useForm<signUpType>({
-        mode: "onBlur",
-        resolver: zodResolver(signUpSchema)
-    }); //links recat hook form to my form
+  const {
+    register,
+    handleSubmit,
+    getFieldState,
+    trigger,
+    formState: { errors },
+  } = useForm<signUpType>({
+    mode: "onBlur",
+    resolver: zodResolver(signUpSchema),
+  });
 
-    const submitForm: SubmitHandler<signUpType> = (data)=>{
-        console.log(data);
+  const submitForm: SubmitHandler<signUpType> = (data) => {
+    console.log(data);
+  };
+
+  const {
+    emailAvailabilityStatus,
+    enteredEmail,
+    checkEmailAvailability,
+    resetCheckEmailAvailability,
+  } = useCheckEmailAvailability();
+
+  const emailOnBlurHandler = async (e: React.FocusEvent<HTMLInputElement>) => {
+    await trigger("email");
+    const value = e.target.value;
+    const { isDirty, invalid } = getFieldState("email");
+
+    if (isDirty && !invalid && enteredEmail !== value) {
+      // checking
+      checkEmailAvailability(value);
     }
 
-    return (
-        <>
-            <Heading title="User Registeration"/>
-            <Row>
-                <Col md={{span: 6, offset: 3}}>
-                    <Form onSubmit={handleSubmit(submitForm)}>
-                        <Input label="First Name" name = "firstName" register={register} error={errors.firstName?.message as string}/>
-                        <Input label="Last Name" name = "lastName" register={register} error={errors.lastName?.message as string}/>
-                        <Input label="Email address" name = "email" register={register} error={errors.email?.message as string}/>
-                        <Input label="Password" type="password" name = "password" register={register} error={errors.password?.message as string}/>
-                        <Input label="Confirm Password" type="password" name = "confirmPassword" register={register} error={errors.confirmPassword?.message as string}/>
+    if (isDirty && invalid && enteredEmail) {
+      resetCheckEmailAvailability();
+    }
+  };
 
-                        <Button variant="info" type="submit" style={{color: "white"}}>
-                            Submit
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
-        </>
-    )
-}
+  return (
+    <>
+      <Heading title="User Registration" />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form onSubmit={handleSubmit(submitForm)}>
+            <Input
+              label="First Name"
+              name="firstName"
+              register={register}
+              error={errors.firstName?.message as string}
+            />
+            <Input
+              label="Last Name"
+              name="lastName"
+              register={register}
+              error={errors.lastName?.message as string}
+            />
+            <Input
+              label="Email Address"
+              name="email"
+              register={register}
+              onBlur={emailOnBlurHandler}
+              error={
+                errors.email?.message
+                  ? errors.email?.message
+                  : emailAvailabilityStatus === "notAvailable"
+                  ? "This email is already in use."
+                  : emailAvailabilityStatus === "failed"
+                  ? "Error from the server."
+                  : ""
+              }
+              formText={
+                emailAvailabilityStatus === "checking"
+                  ? "We're currently checking the availability of this email address. Please wait a moment."
+                  : ""
+              }
+              success={
+                emailAvailabilityStatus === "available"
+                  ? "This email is available for use."
+                  : ""
+              }
+              disabled={emailAvailabilityStatus === "checking" ? true : false}
+            />
+            <Input
+              type="password"
+              label="Password"
+              name="password"
+              register={register}
+              error={errors.password?.message as string}
+            />
+            <Input
+              type="password"
+              label="Confirm Password"
+              name="confirmPassword"
+              register={register}
+              error={errors.confirmPassword?.message as string}
+            />
+            <Button
+              variant="info"
+              type="submit"
+              style={{ color: "white" }}
+              disabled={emailAvailabilityStatus === "checking" ? true : false}
+            >
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default Register;
